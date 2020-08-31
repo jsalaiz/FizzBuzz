91f0f878e8f5734266035761a983d44ef20e0331
Jonathan Salaiz <jonathan.salaiz@gmail.com>
2020-08-31 11:41:25 -0400

    init commit

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numArr = new int[100];
+            for (int i = 1; i < 100; i++)
+            {
+                numArr[i] = i;
+            }
+
+            foreach(var item in numArr)
+            {
+                if (item % 3 == 0)
+                {
+                    if (item % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else if (item % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine($"{item}");
+                }
+            }
+        }
+    }
+}
